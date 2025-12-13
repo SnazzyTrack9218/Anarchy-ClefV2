97d7d9a21cfd923b4072fdadc6d485a26ade3357
SnazzyTrack9218 <72895116+SnazzyTrack9218@users.noreply.github.com>
2025-12-12 20:45:08 -0500

    Add autoplay mode and hunt command

---
+++ src/main/java/adris/altoclef/commands/AutoPlayCommand.java
@@ -0,0 +1,38 @@
+package adris.altoclef.commands;
+
+import adris.altoclef.AltoClef;
+import adris.altoclef.commandsystem.Arg;
+import adris.altoclef.commandsystem.ArgParser;
+import adris.altoclef.commandsystem.Command;
+import adris.altoclef.commandsystem.CommandException;
+
+public class AutoPlayCommand extends Command {
+
+    public AutoPlayCommand() throws CommandException {
+        super("autoplay", "Toggle persistent autoplay survival", new Arg(String.class, "mode", null, 0));
+    }
+
+    @Override
+    protected void call(AltoClef mod, ArgParser parser) throws CommandException {
+        String mode = parser.get(String.class);
+
+        if (mode == null) {
+            mod.toggleAutoPlay();
+            mod.log("Autoplay " + (mod.isAutoPlayEnabled() ? "enabled" : "disabled"));
+            finish();
+            return;
+        }
+
+        if (mode.equalsIgnoreCase("on")) {
+            mod.enableAutoPlay();
+            mod.log("Autoplay enabled");
+        } else if (mode.equalsIgnoreCase("off") || mode.equalsIgnoreCase("stop")) {
+            mod.disableAutoPlay();
+            mod.log("Autoplay disabled");
+        } else {
+            throw new CommandException("Unknown mode '" + mode + "'. Use on/off or no argument to toggle.");
+        }
+
+        finish();
+    }
+}
